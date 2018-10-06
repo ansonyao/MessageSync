7663893b82126cf3af6322d0da64c93a8aa539a5
anson <anson.yao@twotalltotems.com>
2018-10-06 11:56:03 -0700

    Setup

---
+++ messageFetch.py
@@ -0,0 +1,18 @@
+import Lib.ItChat.itchat as itchat
+import time
+from pprint import pprint
+import json
+
+
+def lc(): 
+    print("logged in")
+    messages = itchat.get_msg()
+
+    with open("messages.json", "w") as messageFile:
+        json.dump(messages, messageFile, sort_keys=True, indent=4, separators=(',', ': '))
+        messageFile.close()
+
+itchat.login(loginCallback=lc)
+
+
+
